05289b2a3baebc7abeff8f7fe8e8b0350ce17e27
Adolfo Cuadros <ronnie.adolfo@gmail.com>
2016-10-04 23:39:26 -0500

    Reparación en config database, variables de entorno.

--- config/database.php
+++ config/database.php
@@ -17,9 +17,9 @@ return  [
 
         'mongodb' => [
             'driver'   => 'mongodb',
-            'host'     => env('DB_HOST', 'localhost'),
-            'port'     => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'test'),
+            'host'     => 'localhost',
+            'port'     => '27017',
+            'database' => 'test',
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
             'use_mongo_id' => false,
